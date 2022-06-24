24c88c38058509263fdf32c352715d81ab72d514
danielgsantos15 <danir877@gmail.com>
2022-06-24 19:41:11 -0300

    feat: autores added to index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import app from '../app.js'
 import livros from './livrosRoutes.js'
+import autores from './autoresRoutes.js'
 
 const routes = (app) => {
   app.route('/').get((req, res) => {
@@ -9,7 +9,8 @@ const routes = (app) => {
   
   app.use(
     express.json(),
-    livros
+    livros,
+    autores
     )
 }
 
