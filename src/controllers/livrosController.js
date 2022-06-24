1b576cf55253231c74950eba86d25d70c1070c3f
danielgsantos15 <danir877@gmail.com>
2022-06-24 17:16:09 -0300

    feat: get and put for Id

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -8,6 +8,18 @@ class LivroController {
     })
   }
 
+  static listarLivroPorId = (req, res) => {
+    const id = req.params.id 
+
+    livros.findById(id, (err, livros) => {
+      if(err) {
+        res.status(400).send({message: `${err.message} - Id não encontrada`})
+      } else {
+        res.status(200).send(livros)
+      }
+    })
+  }
+
   static cadastrarLivro = (req, res) => {
     let livro = new livros(req.body)
 
@@ -20,6 +32,19 @@ class LivroController {
       }
     })
   }
+
+  static atualizarLivro = (req, res) => {
+    const id = req.params.id
+
+    livros.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+      if(!err) {
+        res.status(200).send({message: 'Livro atualizado com sucesso!'})
+      } else {
+        res.status(500).send({message: err.message})
+      }
+    })
+  }
+
 }
 
 export default LivroController
