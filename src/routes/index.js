f51ed9c51140c902ecd394219f15114b351f830e
danielgsantos15 <danir877@gmail.com>
2022-06-24 16:24:21 -0300

    feat: path src index

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import app from '../app.js'
+import livros from './livrosRoutes.js'
+
+const routes = (app) => {
+  app.route('/').get((req, res) => {
+    res.status(200).send({titulo: 'Curso de node'})
+  })
+  
+  app.use(
+    express.json(),
+    livros
+    )
+}
+
+export default routes
