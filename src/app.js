a3755f3b270ee9d95161bdc924829fadace0480e
danielgsantos15 <danir877@gmail.com>
2022-06-24 16:26:41 -0300

    refactor: changing routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import db from '../config/dbConnect.js'
 import livros from './models/Livro.js'
+import routes from './routes/index.js'
 
 db.on('error', console.log.bind(console, 'Erro de conexão'))
 db.once('open', () => {
@@ -11,29 +12,16 @@ const app = express()
 
 app.use(express.json())
 
+routes(app)
+
 // const livros = [
 //   {id: 1, 'titulo': 'Harry Potter'},
 //   {id: 2, 'titulo': 'Senhor do Aneis'}
 // ]
 
-app.get('/', (req, res) => {
-  res.status(200).send('Curso de Node')
-})
-
-app.get('/livros', (req, res) => {
-  livros.find((err, livros) => {
-    res.status(200).json(livros)
-  })
-})
-
 app.get('/livros/:id', (req, res) => {
   let index = buscaLivro(req.params.id)
   res.json(livros[index])
-})
-
-app.post('/livros', (req, res) => {
-  livros.push(req.body)
-  res.status(201).send('Livro cadastrado com sucesso!')
 })
 
 app.put('/livros/:id', (req, res) => {
